d735eac15b826eb4986f0d253f48a8fc4531c8de
Eduardo-Lopez85 <eduardo.o.lopez85@gmail.com>
2021-06-07 02:59:12 -0300

    carga ej5y6

--- ejLevel2/ej5.java
+++ ejLevel2/ej5.java
@@ -13,7 +13,35 @@ Output (Salida):
 Total Final: $ 12915
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ej5 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> horastrabajadas = new ArrayList<>();
+        ArrayList<Integer> valorxhora = new ArrayList<>();
+        ArrayList<Integer> pagoxdia = new ArrayList<>();
+        Scanner dato = new Scanner(System.in);
+        int acumulador = 0;
+        //CARGA DE DATOS
+        System.out.println("Ingrese las horas trabajadas de la semana");
+        for (int i=0; i<5; i++){
+            horastrabajadas.add(dato.nextInt());
+        } System.out.println();
+        System.out.println("Ingrese los valores por dia de trabajo");
+        for (int i=0; i<5; i++){
+            valorxhora.add(dato.nextInt());
+        } System.out.println();
+        //Procesamiento
+        for (int i=0; i<5; i++){
+            pagoxdia.add(i,(horastrabajadas.get(i) * valorxhora.get(i)));
+        } System.out.println("Los valores parciales son :");
+        for(int i : pagoxdia){
+            System.out.print(i+"  ");
+            acumulador += i;
+        } System.out.println();
+        System.out.println("El importe a cobrar por la semana calculada es: "+ acumulador);
+
+        dato.close();
+    }
 }
--- ejLevel2/ej6.java
+++ ejLevel2/ej6.java
@@ -10,6 +10,64 @@ Todos los empleados están cargados en un Set (HashSet), se desea calcular el su
 
 */
 
-public class ej6 {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+
+
+class Empleado {
+    String ApyNom;
+    int dni;
+    double horastrabajadas;
+    int valorxhora; 
+
+    public Empleado(String ApyNom, int dni, double horastrabajadas, int valorxhora){
+        this.ApyNom = ApyNom;
+        this.dni = dni;
+        this.horastrabajadas = horastrabajadas;
+        this.valorxhora = valorxhora;
+    }
+
+    public int buscardni(){
+        return dni;
+    }
+
+    public double calcularsalario(){
+        return (horastrabajadas * valorxhora);
+    }
     
+
+}
+
+
+public class ej6 {
+    public static void main(String[] args) {
+
+        Set<Empleado> Empleados = new HashSet<>();
+        Map<Integer, Double> PagoNomina = new HashMap<>();
+
+        Empleado empleado1 = new Empleado("Puente Tito", 11222333, 8.5, 100);
+        Empleado empleado2 = new Empleado("Garcia Juan", 22333444, 7.8, 120);
+        Empleado empleado3 = new Empleado("Gonzalez Teresa", 33444555, 10.2, 150);
+
+        Empleados.add(empleado1);
+        Empleados.add(empleado2);
+        Empleados.add(empleado3);
+
+        for (Empleado emp : Empleados) {
+            PagoNomina.put(emp.buscardni(), emp.calcularsalario());
+        }
+
+        for(Map.Entry<Integer, Double> emp : PagoNomina.entrySet()){
+            System.out.println(emp.getKey() + " : " + emp.getValue());
+        }
+        
+
+        
+
+
+    }
 }
